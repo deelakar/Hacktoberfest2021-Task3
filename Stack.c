1df5c1aacf7cbd5a6737249599744bd00e17217d
Deelaka Rathnayake <deelaka98@gmail.com>
2021-10-03 00:44:26 +0530

    Update Stack.c

--- Stack.c
+++ Stack.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 //Global Variable
-int size,choice,ele;
+int size,choice,ele,i;
 
+void push();
+int pop();
+void display();
+int peek();
 
 //Creating Stack
-struct stack{
+struct stack
+{
     int arr[100];
     int top;
 
@@ -16,14 +21,54 @@ void push(int element)
 {
     if((st.top)==size)
     {
-        printf("\n Stack is Full");
+         printf("\n Enter the Choice:");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+            {
+                push();
+                break;
+            }
+            case 2:
+            {
+                pop();
+                break;
+            }
+            case 3:
+            {
+                peek();
+                break;
+            }
+            case 4:
+            {
+                display();
+                break;
+            }
+            case 5:
+            {
+                printf("\n\t EXIT POINT ");
+                break;
+            }
+            default:
+            {
+                printf ("\n\t Please Enter a Valid Choice(1/2/3/4)");
+            }
+
+        }
+    while(choice!=5);
+    return 0;
     }
     else
     {
-        st.top--;
-        printf("\nEnter a Value ");
-        scanf("%s",&ele);
-        st.arr[st.top]=ele;
+     st.top=-1;
+
+    printf("\n Enter the size of STACK with MAX 100 ");
+    scanf("%d",&size);
+    printf("\n\t STACK OPERATIONS USING ARRAY");
+    printf("\n\t");
+    printf("\n\t 1.PUSH\n\t 2.POP\n\t 3.PEEK\n\t 4.DISPLAY\n\t 5.EXIT");
+    do
     }
 }
 
